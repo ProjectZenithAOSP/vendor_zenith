bba13d3e9c4fcfa7372cf4c7b54797a4e8dbc541
Alvin Francis <nivlafx@gmail.com>
2024-07-22 13:48:34 -0400

    common: Dexpreopt lawnchair launcher
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -218,7 +218,8 @@ endif
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     NexusLauncherRelease \
-    SystemUI
+    SystemUI \
+    Lawnchair
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
