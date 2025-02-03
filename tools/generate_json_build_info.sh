452563cf26d19d88aadd68b3ba4682517e123656
Amrito | アンブロシア <bhabanidas431@gmail.com>
2025-02-03 22:41:47 +0530

    tools: Improve JSON generation script
    * Added Vanilla Support
    * It should Take Zenith Version
    
    Signed-off-by: Amrito | アンブロシア <bhabanidas431@gmail.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2024 The Pixel Project
 # Copyright (C) 2025 Project Zenith
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -16,102 +16,108 @@
 #
 
 # $1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME
-existingOTAjson=./vendor/ota/devices/$1.json
+existingOTAjsonVanilla=./Devices/vanilla/$1.json
+existingOTAjsonGapps=./Devices/gapps/$1.json
 output=$2/$1.json
-version=$4
 
-# cleanup old file
+# Cleanup old file
 if [ -f $output ]; then
-        rm $output
+    rm $output
 fi
 
 echo "Generating JSON file data for OTA support..."
 
-if [ -f $existingOTAjson ]; then
-        # get data from already existing device json
-        maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        filename=$3
-        version=$(grep ro\.zenith\.display\.version ./out/target/product/$1/system/build.prop | cut -d= -f2)
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
-        buildtype=`grep -n "\"buildtype\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        is_active=`grep -n "\"is_active\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$forum" ]; then
-                forum="https:"$forum
-        fi
-        telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$telegram" ]; then
-                telegram="https:"$telegram
-        fi
-        
-        # Extract the device codename
-        codename=$(grep ro\.zenith\.device ./out/target/product/$1/system/build.prop | cut -d= -f2 | xargs)
+# Determine build type based on file name
+if [[ "$3" == *"GAPPS"* ]]; then
+    buildtype="GAPPS"
+elif [[ "$3" == *"VANILLA"* ]]; then
+    buildtype="VANILLA"\else
+    buildtype="UNKNOWN"
+fi
 
-        echo '{
-        "response": [
-                {
-                        "maintainer": "'$maintainer'",
-                        "oem": "'$oem'",
-                        "device": "'$device'",
-                        "codename": "'$codename'",
-                        "version": "'$version'",
-                        "filename": "'$filename'",
-                        "download": "",
-                        "timestamp": '$timestamp',
-                        "md5": "'$md5'",
-                        "sha256": "'$sha256'",
-                        "size": '$size',
-                        "buildtype": "'$buildtype'",
-                        "is_active": "'$is_active'",
-                        "forum": "'$forum'",
-                        "telegram": "'$telegram'"
-                }
-        ]
+# Choose the correct JSON file (prioritize GAPPS if both exist)
+if [ -f $existingOTAjsonGapps ]; then
+    existingOTAjson=$existingOTAjsonGapps
+elif [ -f $existingOTAjsonVanilla ]; then
+    existingOTAjson=$existingOTAjsonVanilla
+else
+    existingOTAjson=""
+fi
+
+
+if [ -n "$existingOTAjson" ]; then
+    # Extract data from the existing device JSON
+    maintainer=$(grep -m1 "\"maintainer\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    oem=$(grep -m1 "\"oem\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    device=$(grep -m1 "\"device\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    filename=$3
+    buildprop=$2/system/build.prop
+    version=$(grep "ro.zenith.display.version" $2/system/build.prop | cut -d '=' -f2 | xargs)
+    timestamp=$(grep "ro.system.build.date.utc" $buildprop | cut -d '=' -f2)
+    md5=$(md5sum "$2/$3" | cut -d ' ' -f1)
+    sha256=$(sha256sum "$2/$3" | cut -d ' ' -f1)
+    size=$(stat -c "%s" "$2/$3")
+    is_active=$(grep -m1 "\"is_active\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    forum=$(grep -m1 "\"forum\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    telegram=$(grep -m1 "\"telegram\"" $existingOTAjson | cut -d ':' -f2- | tr -d '"', | xargs)
+    codename=$(grep "ro.zenith.device" $buildprop | cut -d '=' -f2 | xargs)
+
+    echo '{
+    "response": [
+        {
+            "maintainer": "'$maintainer'",
+            "oem": "'$oem'",
+            "device": "'$device'",
+            "codename": "'$codename'",
+            "version": "'$version'",
+            "filename": "'$filename'",
+            "download": "",
+            "timestamp": '$timestamp',
+            "md5": "'$md5'",
+            "sha256": "'$sha256'",
+            "size": '$size',
+            "buildtype": "'$buildtype'",
+            "is_active": "'$is_active'",
+            "forum": "'$forum'",
+            "telegram": "'$telegram'"
+        }
+    ]
 }' >> $output
 else
-        filename=$3
-        version=`echo $version`
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
+    # Handle unsupported devices
+    filename=$3
+    buildprop=$2/system/build.prop
+    timestamp=$(grep "ro.system.build.date.utc" $buildprop | cut -d '=' -f2)
+    version=$(grep "ro.zenith.display.version" $2/system/build.prop | cut -d '=' -f2 | xargs)
+    md5=$(md5sum "$2/$3" | cut -d ' ' -f1)
+    sha256=$(sha256sum "$2/$3" | cut -d ' ' -f1)
+    size=$(stat -c "%s" "$2/$3")
+    codename=$(grep "ro.zenith.device" $buildprop | cut -d '=' -f2 | xargs)
 
-        # Extract the device codename
-        codename=$(grep ro\.zenith\.device ./out/target/product/$1/system/build.prop | cut -d= -f2 | xargs)
-
-        echo '{
-        "response": [
-                {
-                        "maintainer": "''",
-                        "oem": "''",
-                        "device": "''",
-                        "codename": "'$codename'",
-                        "version": "'$version'",
-                        "filename": "'$filename'",
-                        "download": "",
-                        "timestamp": '$timestamp',
-                        "md5": "'$md5'",
-                        "sha256": "'$sha256'",
-                        "size": '$size',
-                        "buildtype": "''",
-                        "is_active":"''",
-                        "forum": "''",
-                        "telegram": "''"
-                }
-        ]
+    echo '{
+    "response": [
+        {
+            "maintainer": "",
+            "oem": "",
+            "device": "",
+            "codename": "'$codename'",
+            "version": "'$version'",
+            "filename": "'$filename'",
+            "download": "",
+            "timestamp": '$timestamp',
+            "md5": "'$md5'",
+            "sha256": "'$sha256'",
+            "size": '$size',
+            "buildtype": "'$buildtype'",
+            "is_active": "",
+            "forum": "",
+            "telegram": ""
+        }
+    ]
 }' >> $output
 
-        echo 'There is no official support for this device yet'
-        echo 'Apply from https://github.com/ProjectZenithAOSP/Wiki/issues/new?template=device-maintainer-form.yml'
+    echo 'There is no official support for this device yet'
+    echo 'Apply from https://github.com/ProjectZenithAOSP/Wiki/issues/new?template=device-maintainer-form.yml'
 fi
 
 echo ""
