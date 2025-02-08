Evolution-X-AOSP/vendor_evolution

vendor: add OTA script to delete package cache
after flashing new image delete contents of /data/system/package_cache/
to make sure they are recreated. Outdated cache files are the
reason for the famous 'resource derps'

Change-Id: Ia50b8223c07d6eeb09c404599138827be911063b
Signed-off-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -105,6 +105,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/evolution/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.

ProjectZenithAOSP/vendor_zenith

config/common.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Clean up packages cache to avoid wrong strings and resources
+PRODUCT_COPY_FILES += \
+    vendor/zenith/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.