ProjectEverest/vendor_everest

config: Add suport for Vanilla

config/common.mk
@@ -48,10 +48,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Gapps
+# Vanilla and GMS
 WITH_GAPPS ?= true
-ifeq ($(WITH_GAPPS),true)
+ifeq ($(WITH_GAPPS), true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+else
+include vendor/everest/config/vanilla.mk
 endif
 
 # Gboard side padding

ProjectZenithAOSP/vendor_zenith

config/common.mk
@@ -259,8 +259,12 @@ CUSTOM_LOCALES += \
     cy_GB \
     fur_IT
 
-# Google apps and services
-$(call inherit-product, vendor/gms/products/gms.mk)
+# Vanilla and GAPPS
+ifeq ($(WITH_GAPPS), true)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+else
+include vendor/zenith/config/vanilla.mk
+endif
 
 include vendor/zenith/config/version.mk
 