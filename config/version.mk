TPP-Labs/vendor_aosp

vendor: Sign with priv-keys if exists Signed-off-by: RITESH <SahanyRitesh@proton.me>

config/version.mk
@@ -10,3 +10,7 @@ CUSTOM_PLATFORM_VERSION := 15.1
 
 CUSTOM_VERSION := PixelProject_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := fifteen
+
+# Signing
+-include vendor/custom-priv/keys/keys.mk
+

ProjectZenithAOSP/vendor_zenith

config/version.mk
@@ -18,3 +18,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.zenith.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(ZENITH_CODENAME) \
     ro.zenith.releasetype=$(ZENITH_BUILDTYPE) \
     ro.modversion=$(ZENITH_VERSION)
+
+# Signing
+-include vendor/zenith-priv/keys/keys.mk